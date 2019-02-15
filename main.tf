90aaa69e51af5127baf98b5bff190b05150dd325
Keir Badger <keir.badger@acuris.com>
2019-02-15 14:13:44 +0000

    Improve the dependencies

--- main.tf
+++ main.tf
@@ -35,9 +35,11 @@ resource "aws_lambda_permission" "with_sns" {
     source_arn = "${aws_sns_topic.topic.arn}"
 }
 
-resource "aws_cloudwatch_log_group" "example" {
+resource "aws_cloudwatch_log_group" "lambda_loggroup" {
   name              = "/aws/lambda/${var.function_name}"
   retention_in_days = 7
+  depends_on      = ["aws_lambda_function.lambda_function"]
+
 }
 
 
@@ -45,7 +47,7 @@ resource "aws_cloudwatch_log_subscription_filter" "kinesis_log_stream" {
   count           = "${var.datadog_log_subscription_arn != "" ? 1 : 0}"
   name            = "kinesis-log-stream-${var.function_name}"
   destination_arn = "${var.datadog_log_subscription_arn}"
-  log_group_name  = "/aws/lambda/${var.function_name}"
+  log_group_name  = "${aws_cloudwatch_log_group.lambda_loggroup.name}"
   filter_pattern  = ""
-  depends_on      = ["aws_lambda_function.lambda_function"]
+  depends_on      = ["aws_cloudwatch_log_group.lambda_loggroup"]
 }
